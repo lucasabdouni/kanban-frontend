c59ddc2a9d0c4d88c34ebce4d59d82408de8d42b
lucasabdouni <lucasabdouni@hotmail.com>
2024-03-13 19:09:48 -0300

    feat: created kanban dashboard

--- src/app/modules/kanban/dashboard/dashboard.component.ts
+++ src/app/modules/kanban/dashboard/dashboard.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { GetAllCardsResponse } from 'src/app/models/interface/card/GetAllCardsResponse';
+import { GetAllColumnsResponse } from 'src/app/models/interface/column/GetAllColumnsResponse';
+import { CardService } from './../../../service/card/card.service';
+import { ColumnService } from './../../../service/column/column.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject();
+  public columnsDatas: Array<GetAllColumnsResponse> = [];
+  public cardsDatas: Array<GetAllCardsResponse> = [];
 
+  constructor(
+    private columnService: ColumnService,
+    private cardService: CardService
+  ) {}
+
+  ngOnInit(): void {
+    this.getAllColumnsDatas();
+    this.getAllCardsDatas();
+  }
+
+  getAllColumnsDatas() {
+    this.columnService
+      .getAllColumns()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          if (response.length > 0) {
+            this.columnsDatas = response;
+          }
+        },
+        error(err) {
+          console.log('error');
+        },
+      });
+  }
+
+  getAllCardsDatas() {
+    this.cardService
+      .getAllCards()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          if (response.length > 0) {
+            this.cardsDatas = response;
+          }
+        },
+        error(err) {
+          console.log('error');
+        },
+      });
+  }
+
+  getCardsByColumn(columnId: string): GetAllCardsResponse[] {
+    return this.cardsDatas.filter((card) => card.columnsTable.id === columnId);
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
