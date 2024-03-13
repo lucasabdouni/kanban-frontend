43f0dcc72c8e772ac11837850ff56f0a96dfee45
lucasabdouni <lucasabdouni@hotmail.com>
2024-03-13 10:02:42 -0300

    feat: implements auth login service

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,21 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { Subject, takeUntil } from 'rxjs';
+import { AuthRequest } from 'src/app/models/interface/user/auth/AuthRequest';
+import { UserService } from './../../service/user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<void>();
+
   loginForm = this.formBuilder.group({
     email: ['', Validators.required, ''],
     password: ['', Validators.required],
   });
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private cookieService: CookieService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    if (this.userService.isLoggedIn()) {
+      this.router.navigate(['/kanban']);
+    }
+  }
 
   onSubmitLoginForm(): void {
-    console.log(this.loginForm.value);
-    console.log(this.loginForm.valid);
+    if (this.loginForm.value && this.loginForm.valid) {
+      this.userService
+        .auth(this.loginForm.value as AuthRequest)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe({
+          next: (response) => {
+            if (response) {
+              this.cookieService.set('token', response.token);
+              this.router.navigate(['/kanban']);
+            }
+          },
+          error: (error) => {
+            console.log('error');
+          },
+        });
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
