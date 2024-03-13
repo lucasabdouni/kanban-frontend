3371d6f6153b168e36a4b1d31b0413f8d7b775f3
lucasabdouni <lucasabdouni@hotmail.com>
2024-03-13 10:03:29 -0300

    feat: created separate module for kanban and lazzy loading routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guard/auth-guard.service';
 import { HomeComponent } from './modules/home/home.component';
+import { SignUpComponent } from './modules/sign-up/sign-up.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     pathMatch: 'full',
+  },
+  {
+    path: 'register',
+    component: SignUpComponent,
+  },
+  {
+    path: 'kanban',
+    loadChildren: () =>
+      import('./modules/kanban/kanban.module').then((m) => m.KanbanModule),
+    canActivate: [AuthGuard],
   },
 ];
 
