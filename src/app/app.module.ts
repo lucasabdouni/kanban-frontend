d71b0b0c71ddd8f9696ebeb1a0cdf05a8440cf50
lucasabdouni <lucasabdouni@hotmail.com>
2024-03-13 10:00:42 -0300

    chore: imported necessary dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 
+// Modules
+import { GraphQLModule } from './graphql.module';
+
 // Components
+import { CookieService } from 'ngx-cookie-service';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './modules/home/home.component';
+import { SignUpComponent } from './modules/sign-up/sign-up.component';
+import { MessageErrorComponent } from './shared/components/message-error/message-error.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SignUpComponent,
+    MessageErrorComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule,
     ReactiveFormsModule,
+    HttpClientModule,
+    GraphQLModule,
   ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
