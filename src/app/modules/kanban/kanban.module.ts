c5e79504760b065351159a47d526ddff8c05572d
lucasabdouni <lucasabdouni@hotmail.com>
2024-03-13 11:28:49 -0300

    chore: import header component

--- src/app/modules/kanban/kanban.module.ts
+++ src/app/modules/kanban/kanban.module.ts
@@ -1,17 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
-import { KanbanRoutingModule } from './kanban-routing.module';
+import { HeaderComponent } from 'src/app/shared/components/header/header.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
+import { KanbanRoutingModule } from './kanban-routing.module';
 
 @NgModule({
-  declarations: [
-    DashboardComponent
-  ],
-  imports: [
-    CommonModule,
-    KanbanRoutingModule
-  ]
+  declarations: [DashboardComponent, HeaderComponent],
+  imports: [CommonModule, KanbanRoutingModule],
 })
-export class KanbanModule { }
+export class KanbanModule {}
