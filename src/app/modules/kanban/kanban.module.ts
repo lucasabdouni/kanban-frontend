b1d46062a5b6b35dde99b0147a75c52a06803fb7
lucasabdouni <lucasabdouni@hotmail.com>
2024-03-13 19:11:13 -0300

    chore: imported the components into the modules

--- src/app/modules/kanban/kanban.module.ts
+++ src/app/modules/kanban/kanban.module.ts
@@ -4,9 +4,11 @@ import { NgModule } from '@angular/core';
 import { HeaderComponent } from 'src/app/shared/components/header/header.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { KanbanRoutingModule } from './kanban-routing.module';
+import { ColumnsComponent } from './components/columns/columns.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
-  declarations: [DashboardComponent, HeaderComponent],
+  declarations: [DashboardComponent, HeaderComponent, ColumnsComponent, CardComponent],
   imports: [CommonModule, KanbanRoutingModule],
 })
 export class KanbanModule {}
