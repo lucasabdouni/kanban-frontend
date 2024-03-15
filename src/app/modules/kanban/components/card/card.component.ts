7477d0b40778dc2072f554c72a52df590dcf3c5e
lucasabdouni <lucasabdouni@hotmail.com>
2024-03-15 13:14:16 -0300

    feat: created functionality to edit and delete cards

--- src/app/modules/kanban/components/card/card.component.ts
+++ src/app/modules/kanban/components/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Events } from 'src/app/models/enums/Events';
 
 @Component({
   selector: 'app-card',
@@ -10,4 +11,28 @@ export class CardComponent {
   @Input() title!: string;
   @Input() description!: string;
   @Input() responsable!: string;
+  @Output() editCardEvent = new EventEmitter();
+  @Output() deleteCardEvent = new EventEmitter();
+  @Output() alterColumnToCardEvent = new EventEmitter();
+
+  public cardEventEdit = Events.EDIT_CARD_EVENT;
+  public cardAlterColumnEdit = Events.EDIT_COLUMN_TO_CARD;
+
+  handleEditCardEvent(action: string, id: string): void {
+    if (action !== '' && id !== '') {
+      this.editCardEvent.emit({ action, id });
+    }
+  }
+
+  handleCardDeleteEvent(title: string, id: string): void {
+    if (title !== '' && id !== '') {
+      this.deleteCardEvent.emit({ title, id });
+    }
+  }
+
+  handleAlterColumnToCardEvent(action: string, id: string): void {
+    if (action !== '' && id !== '') {
+      this.alterColumnToCardEvent.emit({ action, id });
+    }
+  }
 }
