48787f21d2a8c338fc77a4972b242d21c797ff0e
lucasabdouni <lucasabdouni@hotmail.com>
2024-03-15 13:13:33 -0300

    feat: created methods get, updade and delete in columns service

--- src/app/service/column/column.service.ts
+++ src/app/service/column/column.service.ts
@@ -3,7 +3,9 @@ import { Apollo } from 'apollo-angular';
 import gql from 'graphql-tag';
 import { CookieService } from 'ngx-cookie-service';
 import { Observable, map } from 'rxjs';
-import { GetAllColumnsResponse } from 'src/app/models/interface/column/GetAllColumnsResponse';
+import { CreateColumnRequest } from 'src/app/models/interface/column/request/createColumnRequest';
+import { EditColumnRequest } from 'src/app/models/interface/column/request/editColumnRequest';
+import { GetAllColumnsResponse } from 'src/app/models/interface/column/response/GetAllColumnsResponse';
 
 @Injectable({
   providedIn: 'root',
@@ -36,5 +38,85 @@ export class ColumnService {
       .pipe(
         map((result: any) => result.data.columns as GetAllColumnsResponse[])
       );
+  }
+
+  createColumn({
+    title,
+  }: CreateColumnRequest): Observable<GetAllColumnsResponse> {
+    {
+      return this.apollo
+        .mutate({
+          mutation: gql`
+            mutation CreateColumn($title: String!) {
+              createColumn(data: { title: $title }) {
+                id
+                title
+              }
+            }
+          `,
+          variables: {
+            title,
+          },
+          context: {
+            headers: this.headers,
+          },
+        })
+        .pipe(
+          map(
+            (result: any) => result.data.createColumn as GetAllColumnsResponse
+          )
+        );
+    }
+  }
+
+  editColumn({
+    id,
+    title,
+  }: EditColumnRequest): Observable<GetAllColumnsResponse> {
+    {
+      return this.apollo
+        .mutate({
+          mutation: gql`
+            mutation updateColumn($id: String!, $title: String!) {
+              updateColumn(id: $id, data: { title: $title }) {
+                id
+                title
+              }
+            }
+          `,
+          variables: {
+            title,
+            id,
+          },
+          context: {
+            headers: this.headers,
+          },
+        })
+        .pipe(
+          map(
+            (result: any) => result.data.updateColumn as GetAllColumnsResponse
+          )
+        );
+    }
+  }
+
+  deleteColumn(id: string): Observable<boolean> {
+    {
+      return this.apollo
+        .mutate({
+          mutation: gql`
+            mutation deleteColumn($id: String!) {
+              deleteColumn(id: $id)
+            }
+          `,
+          variables: {
+            id,
+          },
+          context: {
+            headers: this.headers,
+          },
+        })
+        .pipe(map((result: any) => result.data.deleteColumn as boolean));
+    }
   }
 }
