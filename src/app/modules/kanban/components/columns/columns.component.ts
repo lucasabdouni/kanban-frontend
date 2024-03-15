9ee7c85517062dcecdd47ee94f68187daed98d2c
lucasabdouni <lucasabdouni@hotmail.com>
2024-03-15 13:14:59 -0300

    feat: created functionality to edit and delete columns and add cards

--- src/app/modules/kanban/components/columns/columns.component.ts
+++ src/app/modules/kanban/components/columns/columns.component.ts
@@ -1,5 +1,8 @@
-import { Component, Input } from '@angular/core';
-import { GetAllCardsResponse } from 'src/app/models/interface/card/GetAllCardsResponse';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Events } from 'src/app/models/enums/Events';
+import { EventAction } from 'src/app/models/interface/EventAction';
+import { DeleteCardActions } from 'src/app/models/interface/card/actions/DeleteCardActions';
+import { GetAllCardsResponse } from 'src/app/models/interface/card/response/GetAllCardsResponse';
 
 @Component({
   selector: 'app-columns',
@@ -10,4 +13,41 @@ export class ColumnsComponent {
   @Input() title!: string;
   @Input() id!: string;
   @Input() cards: Array<GetAllCardsResponse> = [];
+  @Output() editColumnEvent = new EventEmitter();
+  @Output() deleteColumnEvent = new EventEmitter();
+  @Output() addCardEvent = new EventEmitter();
+  @Output() editCardEvent = new EventEmitter();
+  @Output() deleteCardEvent = new EventEmitter();
+  @Output() alterColumnToCardEvent = new EventEmitter();
+
+  public columnEventEdit = Events.EDIT_COLUMN_EVENT;
+  public AddCardEvent = Events.ADD_CARD_EVENT;
+
+  handleColumnEditEvent(action: string, id: string): void {
+    if (action !== '' && id !== '') {
+      this.editColumnEvent.emit({ action, id });
+    }
+  }
+
+  handleColumnDeleteEvent(title: string, id: string): void {
+    if (title !== '' && id !== '') {
+      this.deleteColumnEvent.emit({ title, id });
+    }
+  }
+
+  handleEditCardEvent(event: EventAction): void {
+    this.editCardEvent.emit(event);
+  }
+
+  handleAddCardEvent(action: string, id: string): void {
+    this.addCardEvent.emit({ action, id });
+  }
+
+  handleDeleteCardEvent(event: DeleteCardActions): void {
+    this.deleteCardEvent.emit(event);
+  }
+
+  handleAlterColumnToCardEvent(event: EventAction): void {
+    this.alterColumnToCardEvent.emit(event);
+  }
 }
